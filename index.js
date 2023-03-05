7c127998b50fb6b23aeaa63860ecdb2575933c6b
Dhruv <dhruv.prajapati@simformsolutions.com>
2023-03-05 23:44:24 +0530

    Eventlisteners configured

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-import './src/main.js';
+import { valuesListener, operationsListener, operatorsListener, specialOperatorsListener, toggleFunction } from "./src/main.js";
+
+document.querySelectorAll('.values').forEach(elem => {
+    elem.addEventListener('click', valuesListener);
+});
+
+document.querySelectorAll('.operations').forEach(elem => {
+    elem.addEventListener('click', operationsListener);
+});
+
+document.querySelectorAll('.operators').forEach(elem => {
+    elem.addEventListener('click', operatorsListener);
+});
+
+document.querySelectorAll('.special-operators').forEach(elem => {
+    elem.addEventListener('click', specialOperatorsListener);
+});
+
+document.querySelectorAll('.toggle-buttons').forEach(elem => {
+    elem.addEventListener('click', toggleFunction);
+});
--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-console.log('Hello World from Main.js');
+export function valuesListener() {
+}
+
+export function operationsListener() {
+}
+
+export function operatorsListener() {
+}
+
+export function specialOperatorsListener() {
+}
+
+export function toggleFunction() {
+}
